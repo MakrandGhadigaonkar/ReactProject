fe3be08db09da401b70a4cfd11887b471d816e44
Makrand Ghadigaonkar <mak.ghadi@gmail.com>
2021-04-25 15:44:25 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eius est
+        cumque eum magni nulla, voluptatibus laudantium blanditiis fuga
+        distinctio, minus repellendus. Illo quidem dolorum ducimus alias
+        perferendis ratione laborum, exercitationem culpa omnis labore et.
+        Suscipit porro voluptatibus facilis fugit omnis hic ipsam assumenda in.
+      </p>
+    </section>
+  )
+}
+
+export default About
